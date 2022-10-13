002c9b1d291d12b74548ced22a68f5965738cadc
Khánh Nguyễn Võ <khanhyou2018@gmail.com>
2022-10-14 00:36:39 +0700

    add

--- DXApplication1/FormRegister.cs
+++ DXApplication1/FormRegister.cs
@@ -14,62 +14,6 @@ namespace DXApplication1
             InitializeComponent();
         }
 
-
-
-
-        /*   private void textBox1_Click(object sender, EventArgs e)
-           {
-
-               txtFristname_Register.Clear();
-
-               picUsername.Image = Properties.Resources.userlogo_before;
-               panel1.ForeColor = Color.FromArgb(78, 184, 206);
-               txtFristname_Register.ForeColor = Color.FromArgb(78, 184, 206);
-
-               if (txtPassword.Text == "" || txtPassword.Text == "Password")
-               {
-                   txtPassword.Text = "Password";
-                   picPassword.Image = Properties.Resources.private_icon1;
-                   panel2.ForeColor = Color.WhiteSmoke;
-                   txtPassword.ForeColor = Color.WhiteSmoke;
-               }
-               else
-               {
-                   picPassword.Image = Properties.Resources.private_icon_before;
-                   panel2.ForeColor = Color.FromArgb(78, 184, 206);
-                   txtPassword.ForeColor = Color.FromArgb(78, 184, 206);
-
-               }
-
-
-           }
-
-           private void textBox2_Click(object sender, EventArgs e)
-           {
-               txtPassword.Clear();
-
-               picPassword.Image = Properties.Resources.private_icon_before;
-               panel2.ForeColor = Color.FromArgb(78, 184, 206);
-               txtPassword.ForeColor = Color.FromArgb(78, 184, 206);
-
-               if (txtFristname_Register.Text == "" || txtFristname_Register.Text == "Username")
-               {
-                   txtFristname_Register.Text = "Username";
-                   picUsername.Image = Properties.Resources.userlogo;
-                   panel1.ForeColor = Color.WhiteSmoke;
-                   txtFristname_Register.ForeColor = Color.WhiteSmoke;
-               }
-               else
-               {
-                   picPassword.Image = Properties.Resources.private_icon_before;
-                   panel1.ForeColor = Color.FromArgb(78, 184, 206);
-                   txtFristname_Register.ForeColor = Color.FromArgb(78, 184, 206);
-
-               }
-
-           }
-        */
-
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
