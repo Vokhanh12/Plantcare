4102c5ea2bcc2b9c03ded4c854ae5dc26ea645e4
Khánh Nguyễn Võ <khanhyou2018@gmail.com>
2022-12-28 23:18:24 +0700

    Working form Register

--- DXApplication1/Statup/Register/FormRegister.cs
+++ DXApplication1/Statup/Register/FormRegister.cs
@@ -14,14 +14,17 @@ namespace DXApplication1
     {
 
         public DateTime thisToday = DateTime.Today;
-        
-       
+
+        public int txtCheckall = 4;
+
+        public string strEmail = null;
+
         public FormRegister()
         {
             InitializeComponent();
         }
-    
- 
+
+
         private void txtLastname_Register_MouseClick(object sender, MouseEventArgs e)
         {
 
@@ -322,110 +325,167 @@ namespace DXApplication1
             }
         }
 
-        public void ProcessString(string s1)
+        public void ProcessString(string s1, string name)
         {
-            if (s1 == null)
-            {
-                throw new ArgumentNullException(paramName: nameof(s1), message: "parameter can't be null.");
-            }
+            string notUseCharacterName = "!@#$%^&*(1234567890_+=-*/';][}{><.,?/~`";
+            string Space = " ";
+            string EmailCharater = "@gmail.com";
 
-         
-        }
+            //For fristname,Lastname
+            if (name == "Fristname" || name == "Lastname")
+                for (int j = 0; j < s1.Length; j++)
+                {
+                    for (int i = 0; i < notUseCharacterName.Length; i++)
+                    {
+                        if (s1[j].ToString() == notUseCharacterName[i].ToString())
+                        {
+                            MessageBox.Show("Không được đặt tên có ký tự đặc biệt " + s1 + "\ntrong " + name);
+                            txtCheckall--;
+                            break;
+                        }
+
+                        if (s1[j].ToString() == Space)
+                        {
+                            MessageBox.Show("Không được để trống " + "trong " + name);
+                            txtCheckall--; 
+                            break;
+                        }
+                    }
+                }
+
+            //For Password,birthday,Email,Username
+            if (name == "Userpassword" || name == "Email" || name == "Ngày sinh"||name=="Username")
+            {
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (s1[i].ToString() == " ")
+                    {
+                        MessageBox.Show("Không được đặt khoảng trống trong " + name);
+                        txtCheckall--;
+                        break;
+                    }
+                }
+            }
+            //For Email khanhyou2018@gmail.com
+            if (name == "Email")
+            {
+                int checkemail=0;
+
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (s1[i].ToString() == "@")
+                    {
+                        strEmail = s1.Substring(i);
+                        if (strEmail != "@gmail.com")
+                        {
+                            MessageBox.Show("Mail không hợp lệ\n Lưu ý chỉ sử đuôi @gmail.com");
+                            strEmail = s1 + "@gmail.com";
+                            txtEmail_Register.Text = strEmail;
+                            txtCheckall--;
+                            break;
+                        }
+                        
+                    }
+                    
+
+                    if (s1[i].ToString() !="@")
+                    {
+                        checkemail++;
+                        if (s1.Length == checkemail)
+                        {
+                            txtEmail_Register.Text = s1 + "@gmail.com";
+                        }
+                    }
+                    
+
+                }
+                
+
+            }
+        } 
 
         public virtual void sendJobApplication()
         {
-            int count = 0;
+             int count = 0;
 
-            string _Fristname = null;
-            string _Lastname = null;
-            string _Username = null;
-            string _Userpassword = null;
-            string _Emailadress = null;
-            string _Birthday = null;
 
-            try
+            
+             var _Fristname = txtFristname_Register.Text;
+             var _Lastname = txtLastname_Register.Text;
+             var _Username = txtUsername_Register.Text;
+             var _Userpassword = txtPassword_Register.Text;
+             var _Emailadress = txtEmail_Register.Text;
+             var _Birthday = dE_Brithday.Text;
+
+             ProcessString(_Fristname,"Fristname");
+             ProcessString(_Lastname,"Lastname");
+             ProcessString(_Username,"Username");
+             ProcessString(_Userpassword,"Userpassword");
+             ProcessString(_Emailadress,"Email");
+             ProcessString(_Birthday,"Ngày sinh");
+
+            if (txtCheckall == 4)
             {
-                     _Fristname = txtFristname_Register.Text;
-                     _Lastname = txtLastname_Register.Text;
-                     _Username = txtUsername_Register.Text;
-                     _Userpassword = txtPassword_Register.Text;
-                     _Emailadress = txtEmail_Register.Text;
-                     _Birthday = dE_Brithday.Text;
-
-                    ProcessString(_Fristname);
-                    ProcessString(_Lastname);
-                    ProcessString(_Username);
-                    ProcessString(_Userpassword);
-                    ProcessString(_Emailadress);
-                    ProcessString(_Birthday);
-
-
-            } catch(Exception ex)
-            {
-                MessageBox.Show("code:" +ex);
-            }
-
-            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["LAPTOP-JN4FK6OT"].ConnectionString))
-            {
-
-                if (db.State == ConnectionState.Closed)
-                    db.Open();
-
-                //Return a count of the table DATA_APPLICATION_REGISTER and column USERNAME
-                var data = db.Query<RegisterDTO>("SELECT COUNT (UserName) as 'RETURN_COUNTCOLUMN' from DATA_APPLICATION_REGISTER ", commandType: CommandType.Text);
-                //Select DATA OF APPLLCATION_REGISTER
-                var data1 = db.Query<RegisterDTO>("SELECT UserName FROM DATA_APPLICATION_REGISTER", commandType: CommandType.Text);
-
-                foreach (RegisterDTO p in data1)
+                using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["LAPTOP-JN4FK6OT"].ConnectionString))
                 {
-                    if (_Username == p.UserName)
+
+                    if (db.State == ConnectionState.Closed)
+                        db.Open();
+
+                    //Return a count of the table DATA_APPLICATION_REGISTER and column USERNAME
+                    var data = db.Query<RegisterDTO>("SELECT COUNT (UserName) as 'RETURN_COUNTCOLUMN' from DATA_APPLICATION_REGISTER ", commandType: CommandType.Text);
+                    //Select DATA OF APPLLCATION_REGISTER
+                    var data1 = db.Query<RegisterDTO>("SELECT UserName FROM DATA_APPLICATION_REGISTER", commandType: CommandType.Text);
+
+                    foreach (RegisterDTO p in data1)
                     {
-                        MessageBox.Show("Đã có người đặt tên tài khoảng này.Vui lòng đặt tên khác");
-                        break;
-                    }
-
-                    if (_Userpassword == p.UserPassword)
-                    {
-                        MessageBox.Show("Đã có người đặt mật khẩu này.Vui lòng đặt mật khẩu khác");
-                        break;
-                    }
-
-                    if (_Emailadress == p.EmailAdress)
-                    {
-                        MessageBox.Show("Email này đã có rồi.Vui lòng chọn Email khác");
-                        break;
-                    }
-
-                    count++;
-
-                }
-
-                foreach (RegisterDTO p in data)
-                {
-                    if (count == p.RETURN_COUNTCOLUMN)
-                    {
-                        try
+                        if (_Username == p.UserName)
                         {
-                            var dataAdd = db.ExecuteAsync($"INSERT INTO DATA_APPLICATION_REGISTER VALUES('{_Username}',PWDENCRYPT('{_Userpassword}'),'{_Fristname}','{_Lastname}','{_Emailadress}','{_Birthday}','{thisToday.ToString("d")}',null,'{"CHUA DUYET"}')");
-                            MessageBox.Show("Tạo tài khoảng thành công.Vui lòng liên hệ");
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show("ERRO: FORM REGISTER code:" + ex);
+                            MessageBox.Show("Đã có người đặt tên tài khoảng này.Vui lòng đặt tên khác");
+                            break;
                         }
 
+                        if (_Userpassword == p.UserPassword)
+                        {
+                            MessageBox.Show("Đã có người đặt mật khẩu này.Vui lòng đặt mật khẩu khác");
+                            break;
+                        }
+
+                        if (_Emailadress == p.EmailAdress)
+                        {
+                            MessageBox.Show("Email này đã có rồi.Vui lòng chọn Email khác");
+                            break;
+                        }
+
+                        count++;
+
                     }
+
+                    foreach (RegisterDTO p in data)
+                    {
+                        if (count == p.RETURN_COUNTCOLUMN)
+                        {
+                            try
+                            {
+                                var dataAdd = db.ExecuteAsync($"INSERT INTO DATA_APPLICATION_REGISTER VALUES('{_Username}',PWDENCRYPT('{_Userpassword}'),'{_Fristname}','{_Lastname}','{_Emailadress}','{_Birthday}','{thisToday.ToString("d")}',null,'{"CHUA DUYET"}')");
+                                MessageBox.Show("Tạo tài khoảng thành công.Vui lòng liên hệ");
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("ERRO: FORM REGISTER code:" + ex);
+                            }
+
+                        }
+                    }
+
                 }
-
-
-
-
-
-
-
-
-
             }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại " + txtCheckall);
+            }
+
+            
 
         }
 
