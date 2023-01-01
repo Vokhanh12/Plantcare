b16a2225142559dfd4e517a1a456be5aa85fb693
Khánh Nguyễn Võ <khanhyou2018@gmail.com>
2023-01-01 16:50:11 +0700

    Add

--- DXApplication1/BrowseJob/sqlOpen.cs
+++ DXApplication1/BrowseJob/sqlOpen.cs
@@ -94,18 +94,18 @@ namespace DXApplication1.BrowseJob
 
 
 
-            string ID = newFormSave.instance.txtID_A.Text;
-            string Usercode = newFormSave.instance.txtUsercode_A.Text;
-            string Nameuser = newFormSave.instance.txtNameUser_A.Text;
-            string Username = newFormSave.instance.txtUsername_A.Text;
-            string Userpassword = newFormSave.instance.txtUserpassword_A.Text;
-            string Fristname = newFormSave.instance.txtFristName_A.Text;
-            string Lastname = newFormSave.instance.txtLastName_A.Text;
-            string EmailAdress = newFormSave.instance.txtEmail_A.Text;
-            string SDT_USER = newFormSave.instance.txtSDT_A.Text;
-            string DateofBirth = newFormSave.instance.txtDOB_A.Text;
-            string NGAY_DANG_KY = newFormSave.instance.txtNDK_A.Text;
-            string TAI_KHOANG = newFormSave.instance.cbBrowse_A.Text;
+            string _ID = newFormSave.instance.txtID_A.Text;
+            string _Usercode = newFormSave.instance.txtUsercode_A.Text;
+            string _Nameuser = newFormSave.instance.txtNameUser_A.Text;
+            string _Username = newFormSave.instance.txtUsername_A.Text;
+            string _Userpassword = newFormSave.instance.txtUserpassword_A.Text;
+            string _Fristname = newFormSave.instance.txtFristName_A.Text;
+            string _Lastname = newFormSave.instance.txtLastName_A.Text;
+            string _EmailAdress = newFormSave.instance.txtEmail_A.Text;
+            string _SDT_USER = newFormSave.instance.txtSDT_A.Text;
+            string _DateofBirth = newFormSave.instance.txtDOB_A.Text;
+            string _NGAY_DANG_KY = newFormSave.instance.txtNDK_A.Text;
+            string _TAI_KHOANG = newFormSave.instance.cbBrowse_A.Text;
 
 
 
@@ -117,52 +117,107 @@ namespace DXApplication1.BrowseJob
                     db.Open();
 
                 // get item in DATA_APPLICATION_USER
-                var dataAP_USER = db.ExecuteAsync($"SET IDENTITY_INSERT dbo.DATA_APPLICATION_USER ON INSERT INTO dbo.DATA_APPLICATION_USER(ID,UserName,UserPassword,TAI_KHOANG) SELECT '{ID}','{Username}','{Userpassword}','{TAI_KHOANG}' SET IDENTITY_INSERT dbo.DATA_APPLICATION_USER OFF", commandType: CommandType.Text);
+                  var dataAP_USER = db.ExecuteAsync($"SET IDENTITY_INSERT dbo.DATA_APPLICATION_USER ON INSERT INTO dbo.DATA_APPLICATION_USER(ID,UserName,UserPassword,TAI_KHOANG) SELECT '{_ID}','{_Username}','{_Userpassword}','{_TAI_KHOANG}' SET IDENTITY_INSERT dbo.DATA_APPLICATION_USER OFF", commandType: CommandType.Text);
 
-                /* var DataUsername = db.Query<BrowseJobDTO>("SELECT ncc.ID,mcc.UserName FROM DATA_APPLICATION_REGISTER mcc,DATA_APPLICATION_USER ncc WHERE ncc.UserName=mcc.UserName", commandType: CommandType.Text);
+                  var DataUsername = db.Query<BrowseJobDTO>("SELECT ncc.ID,mcc.UserName FROM DATA_APPLICATION_REGISTER mcc,DATA_APPLICATION_USER ncc WHERE ncc.UserName=mcc.UserName", commandType: CommandType.Text);
 
                  foreach (BrowseJobDTO p in DataUsername)
                  {
-                     if (Username == p.UserName)
+                     if (_Username == p.UserName)
                      {
                          MessageBox.Show("Đã có người đặt Username này");
                          break;
                      }
-                     else if (ID == p.ID)
+                     else if (_ID == p.ID)
                      {
                          MessageBox.Show("Đã có người đặt ID này");
                          break;
                      }
-                     else if (TAI_KHOANG == "Manager")
+                     else if (_TAI_KHOANG == "Manager")
                      {
-                         var dataManager = db.ExecuteAsync($"SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_EMPLOYEE ON INSERT INTO dbo.DATA_APPLICATION_FOR_EMPLOYEE (ID,MaNV,TenNV,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_EMPLOYEE,TAI_KHOANG,NGAY_DANG_KY) SELECT DATA_APPLICATION_USER.ID,'{Usercode}','{Nameuser}', DATA_APPLICATION_USER.UserName, DATA_APPLICATION_USER.UserPassword,'{Fristname}','{Lastname}','{EmailAdress}','{SDT_USER}',DATA_APPLICATION_USER.TAI_KHOANG,'{NGAY_DANG_KY}' FROM DATA_APPLICATION_USER WHERE DATA_APPLICATION_USER.ID={ID} and DATA_APPLICATION_USER.UserName='{Username}'; SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_EMPLOYE OFF ", commandType: CommandType.Text);
-                         MessageBox.Show("Duyệt thành công");
-                         break;
+                        var dataMANAGER = db.ExecuteAsync($" SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_MANAGER ON " +
+                                                           " INSERT INTO dbo.DATA_APPLICATION_FOR_MANAGER (ID,MaQL,TenQL,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_MANAGER,TAI_KHOANG,NGAY_DANG_KY) " +
+                                                           " OUTPUT inserted.ID, inserted.MaQL,inserted.TenQL, inserted.UserName,inserted.UserPassword,inserted.FristName,inserted.LastName,inserted.FristName,inserted.SDT_MANAGER,inserted.TAI_KHOANG,inserted.NGAY_DANG_KY " +
+                                                         $" SELECT DATA_APPLICATION_USER.ID,'{_Usercode}','{_Nameuser}', DATA_APPLICATION_USER.UserName, DATA_APPLICATION_USER.UserPassword,'{_Fristname}','{_Lastname}','{_EmailAdress}','{_SDT_USER}',DATA_APPLICATION_USER.TAI_KHOANG,'{_NGAY_DANG_KY}' FROM dbo.DATA_APPLICATION_REGISTER,dbo.DATA_APPLICATION_USER WHERE DATA_APPLICATION_USER.ID=9 AND DATA_APPLICATION_REGISTER.UserName='{_Username}'", commandType: CommandType.Text);
+                        MessageBox.Show("Duyệt thành công");
+                        break;
                      }
-                     else if (TAI_KHOANG == "Employee")
+                     else if (_TAI_KHOANG == "Employee")
                      {
-                         var dataEmployee = db.ExecuteAsync($"SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_MANAGER ON INSERT INTO dbo.DATA_APPLICATION_FOR_MANAGER (ID,MaQL,TenQL,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_MANAGER,TAI_KHOANG,NGAY_DANG_KY) SELECT DATA_APPLICATION_USER.ID,'{Usercode}','{Nameuser}', DATA_APPLICATION_USER.UserName, DATA_APPLICATION_USER.UserPassword,'{Fristname}','{Lastname}','{EmailAdress}','{SDT_USER}',DATA_APPLICATION_USER.TAI_KHOANG,'{NGAY_DANG_KY}' FROM DATA_APPLICATION_USER WHERE DATA_APPLICATION_USER.ID={ID} and DATA_APPLICATION_USER.UserName='{Username}'; SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_EMPLOYE OFF ", commandType: CommandType.Text);
-                         MessageBox.Show("Duyệt thành công");
-                         break;
+                        var dataEMPLOYEE = db.ExecuteAsync($" SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_EMPLOYEE ON " +
+                                                         " INSERT INTO dbo.DATA_APPLICATION_FOR_EMPLOYEE (ID,MaNV,TenNV,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_EMPLOYEE,TAI_KHOANG,NGAY_DANG_KY) " +
+                                                         " OUTPUT inserted.ID,inserted.MaNV,inserted.TenNV,inserted.UserName,inserted.UserPassword,inserted.FristName,inserted.LastName,inserted.FristName,inserted.SDT_EMPLOYEE,inserted.TAI_KHOANG,inserted.NGAY_DANG_KY " +
+                                                       $" SELECT DATA_APPLICATION_USER.ID,'{_Usercode}','{_Nameuser}', DATA_APPLICATION_USER.UserName, DATA_APPLICATION_USER.UserPassword,'{_Fristname}','{_Lastname}','{_EmailAdress}','{_SDT_USER}',DATA_APPLICATION_USER.TAI_KHOANG,'{_NGAY_DANG_KY}' FROM dbo.DATA_APPLICATION_REGISTER,dbo.DATA_APPLICATION_USER WHERE DATA_APPLICATION_USER.ID=9 AND DATA_APPLICATION_REGISTER.UserName='{_Username}'", commandType: CommandType.Text);
+                        MessageBox.Show("Duyệt thành công");
+                        break;
                      }
 
 
 
                  }
-                */
-
-                var dataManager = db.ExecuteAsync($"SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_EMPLOYEE ON INSERT INTO dbo.DATA_APPLICATION_FOR_EMPLOYEE (ID,MaNV,TenNV,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_EMPLOYEE,TAI_KHOANG,NGAY_DANG_KY) SELECT DATA_APPLICATION_USER.ID,'{Usercode}','{Nameuser}', DATA_APPLICATION_USER.UserName, DATA_APPLICATION_USER.UserPassword,'{Fristname}','{Lastname}','{EmailAdress}','{SDT_USER}',DATA_APPLICATION_USER.TAI_KHOANG,'{NGAY_DANG_KY}' FROM DATA_APPLICATION_USER WHERE DATA_APPLICATION_USER.ID={ID} and DATA_APPLICATION_USER.UserName='{Username}'", commandType: CommandType.Text);
-                MessageBox.Show("Duyệt thành công");
+                
 
 
 
-                db.Close();
+              //  var dataEmployee = db.ExecuteAsync($" SET IDENTITY_INSERT dbo.DATA_APPLICATION_FOR_MANAGER ON " +
+              //  " INSERT INTO dbo.DATA_APPLICATION_FOR_MANAGER (ID,MaQL,TenQL,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_MANAGER,TAI_KHOANG,NGAY_DANG_KY) " +
+              //  " OUTPUT inserted.ID, inserted.MaQL, inserted.UserName,inserted.UserPassword,inserted.FristName,inserted.LastName,inserted.FristName,inserted.SDT_MANAGER,inserted.TAI_KHOANG,inserted.NGAY_DANG_KY " +
+              //" SELECT DATA_APPLICATION_USER.ID,'QL03','NGUYEN TRUNG KHANH', DATA_APPLICATION_USER.UserName, DATA_APPLICATION_USER.UserPassword,'NGUYEN','KHANH','KHANH2002@gmail.com','0909090909',DATA_APPLICATION_USER.TAI_KHOANG,'12/21/2022' FROM dbo.DATA_APPLICATION_REGISTER,dbo.DATA_APPLICATION_USER WHERE DATA_APPLICATION_USER.ID=9 AND DATA_APPLICATION_REGISTER.UserName='rongchoi9'", commandType: CommandType.Text);
+              //  MessageBox.Show("Duyệt thành công");
+              //  db.Close();
 
             }
         }
 
+            //string conString = "Server=ServerName;Database=DBName;User Id=UserName;Password =********; ";
+            //string sqlcmd = " INSERT INTO dbo.DATA_APPLICATION_FOR_MANAGER (ID,MaQL,TenQL,UserName,UserPassword,FristName,LastName,EmailAdress,SDT_MANAGER,TAI_KHOANG,NGAY_DANG_KY) "
+            //              + " OUTPUT inserted.ID, inserted.Name, inserted.CreatedDate "
+            //              + " SELECT TOP 10 [ID], [Name], [CreatedDate] FROM [TestDB].[dbo].[TempOrig] ";
+
+            /*
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlCommand cmd = new SqlCommand(sqlcmd, con))
+                {
+                    con.Open();
+
+                    // fill in the DataTable object
+                    DataTable dt = new DataTable();
+                    using (var insertedOutput = cmd.ExecuteReader())
+                    {
+                        dt.Load(insertedOutput);
+                    }
+
+
+                    // Read from DataTable object and print on the console
+                    DataRow[] currentRows = dt.Select(null, null);
+
+                    if (currentRows.Length < 1)
+                        Console.WriteLine("No Current Rows Found");
+                    else
+                    {
+                        foreach (DataColumn column in dt.Columns)
+                            Console.Write("\t{0}", column.ColumnName);
+
+                        Console.WriteLine("\t");
+
+                        foreach (DataRow row in currentRows)
+                        {
+                            foreach (DataColumn column in dt.Columns)
+                                Console.Write("\t{0}", row[column]);
+
+                            Console.WriteLine("\t");
+                        }
+                    }
+                    Console.ReadLine();
+                }
+            }
+            */
+
     }
+
 }
+
 
 
 
